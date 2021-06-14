25a12b5b26554dc1b9cdc6d51d8818d809d39cb2
YoshinoriKamiya <78638073+YoshinoriKamiya@users.noreply.github.com>
2021-06-14 21:24:29 +0900

    Signup login (#2)
    * created login & sinup
    
    * made changes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+  before_action :only_loggedin_users, only: [:edit, :update]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Saved successfully"
+      redirect_to login_url
+    else
+      flash[:danger] = "Invaild content. Try again"
+      render 'new'
+    end
+  end
+
+  def Update
+  end
+ 
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+ end
+   
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(roo_url) unless current_user?(@user)
+  end
+end
